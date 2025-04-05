75313ee5e21680c4fd1f4876358b94dc26af8d9e
Colby <cvertilus@fi.uba.ar>
2025-04-05 16:44:10 -0300

    Global error Response para las solicitudes

---
+++ account-service/account-service/src/main/java/com/digitalMoney/demo/Config/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.digitalMoney.demo.Config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String error;
+    private String message;
+}
