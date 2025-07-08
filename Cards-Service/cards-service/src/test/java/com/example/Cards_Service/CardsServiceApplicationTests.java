219e0adb58e67e001cf93ff072be06492137a2c9
Colby <cvertilus@fi.uba.ar>
2025-07-08 13:59:40 -0300

    Cambios

---
+++ Cards-Service/cards-service/src/test/java/com/example/Cards_Service/CardsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cards_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
